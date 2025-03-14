51079dd9831018bbde8452d23a5b79bd15a61c4e
Cauã Augusto <cauaaugusto2.00@gmail.com>
2025-03-14 16:00:33 -0300

    Update menu 2.0

--- front-end/js/swiper.js
+++ front-end/js/swiper.js
@@ -1,9 +1,29 @@
 var swiper = new Swiper(".swiper", {
+    loop:true,
+    grabcursor:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
         el: ".swiper-pagination",
+        clickable:true
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      500: {
+        slidesPerView: 1,
+        spaceBetween: 18
       },
-  });
+      // when window width is >= 480px
+      820: {
+        slidesPerView: 2,
+        spaceBetween: 18
+      },
+      // when window width is >= 640px
+      1120: {
+        slidesPerView: 2,
+        spaceBetween: 24
+      }
+    }
+});
