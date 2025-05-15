6a8f9cd43616bf885f881b1693bc23793b8e7628
Cauã Augusto <cauaaugusto2.00@gmail.com>
2025-05-15 12:08:27 -0300

    Update testes

--- front-end-old/js/swiper.js
+++ front-end-old/js/swiper.js
@@ -1,29 +1,26 @@
 var swiper = new Swiper(".swiper", {
-    loop:true,
-    grabcursor:true,
+    loop: true,
+    grabCursor: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
         el: ".swiper-pagination",
-        clickable:true
+        clickable: true
     },
     breakpoints: {
-
-      500: {
-        slidesPerView: 1,
-        spaceBetween: 18
-      },
-     
-      820: {
-        slidesPerView: 2,
-        spaceBetween: 18
-      },
-      
-      1120: {
-        slidesPerView: 2,
-        spaceBetween: 24
-      }
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 18
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 18
+        },
+        1188: {
+            slidesPerView: 3,
+            spaceBetween: 24
+        }
     }
 });
